e0f3cbc746677ff63e2fb3ebf3f3ec975c767d94
Aerodynale <abrune356@gmail.com>
2024-02-29 19:29:32 +0100

    1

--- launch_simulation - Copia.sh
+++ launch_simulation - Copia.sh
@@ -4,7 +4,8 @@
 #$ -N Train4
 #$ -S /bin/bash
 #$ -q all.q                 # queueName
-#$ -l h_rt=70:00:00
+#$ -pe mpi 1        # cpuNumber
+#$ -l h_rt=90:00:00
 
 module use /software/spack/spack/share/spack/modules/linux-rocky8-sandybridge/
 module load openfoam
@@ -18,7 +19,7 @@ cp -r 0.orig 0
 surfaceFeatureExtract > logs/01_surfaceFeature.log 2>&1
 blockMesh > logs/02_blockMesh.log 2>&1
 decomposePar > logs/03_decompose.log 2>&1
-snappyHexMesh -overwrite > logs/04_snappyHexMesh.log 2>&1
+mpirun --hostfile machinefile.$JOB_ID -np 1 snappyHexMesh -parallel -overwrite > logs/04_snappyHexMesh.log 2>&1
 topoSet > logs/05_topoSet.log 2>&1
 createPatch -overwrite > logs/06_createPatch.log 2>&1
 reconstructParMesh -constant > logs/07_reconstruct.log 2>&1
@@ -31,8 +32,8 @@ rm -r 0
 cp -r 0.orig 0
 renumberMesh -overwrite > logs/09_renumberMesh.log 2>&1
 decomposePar > logs/10_decomposePar_secondaIterazione.log 2>&1
-potentialFoam > logs/11_potentialFoam.log 2>&1
-simpleFoam > logs/12_simpleFoam.log 2>&1
+mpirun --hostfile machinefile.$JOB_ID -np 1 potentialFoam -parallel > logs/11_potentialFoam.log 2>&1
+mpirun --hostfile machinefile.$JOB_ID -np 1 simpleFoam -parallel > logs/12_simpleFoam.log 2>&1
 reconstructParMesh -constant > logs/13_reconstructParMesh.log 2>&1
 reconstructPar -latestTime > logs/14_reconstructPar.log 2>&1
 
